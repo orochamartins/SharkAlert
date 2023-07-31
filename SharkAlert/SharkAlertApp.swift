0c2f4016f3615d4ccb0757d376ed9a2a06f83954
Renato Martins <rochamartinsrenato@gmail.com>
2023-07-31 22:38:07 +0100

    Initial Commit

---
+++ SharkAlert/SharkAlertApp.swift
@@ -0,0 +1,17 @@
+//
+//  SharkAlertApp.swift
+//  SharkAlert
+//
+//  Created by Renato Martins on 31/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct SharkAlertApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
